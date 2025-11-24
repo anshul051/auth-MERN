5719930f7e98247a105e194bcea3a57c1e0377fa
anshul051 <anshulkumararya51@gmail.com>
2025-11-24 23:40:27 +0530

    Fixed CORS for Vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/auth.js";
+import cors from "cors";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://auth-mern-woad.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 app.use(express.json());
 
